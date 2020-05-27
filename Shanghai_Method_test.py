c5281ebfd0610b567d38c0f222e028fc58485a4e
Thomas James <tomjamescontact@gmail.com>
2020-05-27 21:38:03 +0100

    Feat: Index proposed

--- Shanghai_Method_test.py
+++ Shanghai_Method_test.py
@@ -1,21 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-"""
-MSI Mask Shanghai
-"""
-# Load the data and extract the bands
-DEM = np.load("Shanghai/DEM-Region2.npy")
-DEM = DEM[-1]
-# DEM[DEM > 0.0] = 1
-# DEM[DEM < 0.0] = 0
+import math
+from PIL import Image
 
-SAR = np.load("Shanghai/SAR-Region2.npy")
+
+SAR = np.load("Shanghai/SAR-Region4.npy")
 SAR = SAR[-1][:, :, 0]
 # SAR[SAR > 0.1] = 1
 # SAR[SAR < 0.1] = 0
 
-MSI = np.load("Shanghai/MSI-Region2.npy")
+MSI = np.load("Shanghai/MSI-Region4.npy")
 blue = MSI[-1][:, :, 1]
 green = MSI[-1][:, :, 2]
 red = MSI[-1][:, :, 3]
@@ -23,16 +18,30 @@ NIR = MSI[-1][:, :, 7]
 SWIR1 = MSI[-1][:, :, 10]
 SWIR2 = MSI[-1][:, :, 11]
 
+NDBI = (SWIR2 - NIR)/ (SWIR2 + NIR)
+NDBI[NDBI > 0] = 255
+NDBI[NDBI < 0] = 0
+
+plt.imshow(NDBI)
+plt.show()
+
+
 # Decide on an index
-PI = ((blue - SWIR2) / (blue + SWIR2))
+PI = (((SWIR2 - NIR) / (SWIR2 + NIR)) + ((green - SWIR2) / (green + SWIR2)) + ((green - NIR) / (green + NIR)))
 
 # Interpolate the index
-PI[PI < 0] = 0
-PI[PI > 0] = 1
 
-# A = (PI - SAR) / (PI + SAR)
+PI[PI > 0] = 255
+PI[PI < 0] = 0
 
 plt.imshow(PI)
 plt.show()
 
-plt.imsave("shangai.png", PI)
+
+TC = np.load("Shanghai/TC-Region4.npy")
+print(TC.shape)
+TC = TC[-1]
+
+
+plt.imsave("PI-shangai.png", PI, cmap="Blues")
+plt.imsave("TC-shangai.png", TC)
